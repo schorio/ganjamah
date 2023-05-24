d35ad600059589a37437cdb699b1488c79846d71
schorio <schorioignace@gmail.com>
2023-05-24 09:33:40 +0300

    Premier commit avec restaurant et hotel

---
+++ includes/sidebar.php
@@ -0,0 +1,68 @@
+<div class="sidebar" id="sidebar">
+                <div class="sidebar-inner slimscroll">
+					<div id="sidebar-menu" class="sidebar-menu">
+						<ul>
+							<!-- <li class="menu-title"> 
+								<span>Principale</span>
+							</li>
+
+
+							<li class="submenu">
+								<a href="#"><i class="la la-dashboard"></i> <span> Dashboard</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="/ganjamah/index.php"> -- Accueil</a></li>
+									<li><a href="/ganjamah/employee-dashboard.php"> -- Mon dashboard</a></li>
+								</ul>
+							</li> -->
+							
+
+							<li class="menu-title"> 
+								<span>Section</span>
+							</li>
+
+							<li> 
+								<a href="/ganjamah/liste/hotel.php"><i class="la la-hotel"></i> <span>Hrotel</span></a>
+							</li>
+
+							<li> 
+								<a href="/ganjamah/liste/restaurant.php"><i class="la la-resto"></i> <span>Restaurant</span></a>
+							</li>
+							<!-- <li class="submenu">
+								<a href="#" class="noti-dot"><i class="la la-user"></i> <span> Listes</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="/ganjamah/liste/employee/employees.php"> -- Employés</a></li>
+									<li><a href="/ganjamah/liste/departement/departement.php"> -- Departements</a></li>
+									<li><a href="/ganjamah/liste/etablissement/etablissement.php"> -- Etablissements</a></li>
+									<li><a href="/ganjamah/liste/categorie/categorie.php"> -- Categorie</a></li>
+									<li><a href="/ganjamah/liste/statut/statut.php"> -- Statut</a></li>
+									<li><a href="/ganjamah/liste/grade/grade.php"> -- Grade</a></li>
+								</ul>
+							</li> -->
+
+
+							<li class="menu-title"> 
+								<span>Autre</span>
+							</li>
+							<li> 
+								<a href="/ganjamah/includes/function/utilisateur/changer_mdp.php"><i class="la la-cogs"></i> <span>Paramètre</span></a>
+							</li>
+
+							<li class="menu-title"> 
+								<span>Administrateur</span>
+							</li>
+							<li> 
+								<a href="/ganjamah/liste/utilisateur/ut.php"><i class="la la-user-plus"></i> <span>Utilisateurs</span></a>
+							</li>
+
+
+							<li class="menu-title"> 
+								<span>Sortir</span>
+							</li>
+							<li> 
+								<a href="/ganjamah/includes/function/connection/logout.php"><i class="la la-power-off"></i> <span>Déconnection</span></a>
+							</li>
+									
+						</ul>
+					</div>
+                </div>
+            </div>
