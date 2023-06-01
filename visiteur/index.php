cbdb31fd5f4c72acb28960ce6ca843dc067111f0
schorio <schorioignace@gmail.com>
2023-06-01 18:19:03 +0300

    style visiteur : modif url de fontawesome

--- visiteur/index.php
+++ visiteur/index.php
@@ -8,9 +8,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- font awesome cdn -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/ganjamah/assets/font-awesome-6/css/all.css">
     <!-- fonts -->
     <link rel="stylesheet" href="font/fonts.css">
     <!-- normalize css -->
