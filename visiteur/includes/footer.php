76b605053030c5fa21d51550f5e2bc880bb2c32d
schorio <schorioignace@gmail.com>
2023-06-01 19:44:50 +0300

    footer : separation du code qui affiche le footer

---
+++ visiteur/includes/footer.php
@@ -0,0 +1,41 @@
+<footer class="py-2">
+        <div class="container footer-row">
+            <div class="footer-item">
+                <a href="index.php" class="site-brand">
+                    Ganja<span>Mah.</span>
+                </a>
+            </div>
+
+            <div class="footer-item">
+                <h2>Follow us on: </h2>
+                <ul class="social-links">
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-pinterest"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-google-plus"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </footer>
