286a0f91c3d757d2c363a7f79be457b6dbaef80e
schorio <schorioignace@gmail.com>
2023-06-15 07:49:05 +0300

    suppresion des sous titres dans les headers

--- visiteur/blog.php
+++ visiteur/blog.php
@@ -29,7 +29,7 @@
             <div class = "container">
                 <div class = "header-title">
                     <h1>Blog</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus rerum maxime enim odit illum in molestias beatae doloremque, ratione optio.</p>
+                    <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus rerum maxime enim odit illum in molestias beatae doloremque, ratione optio.</p> -->
                 </div>
             </div>
         </header>
