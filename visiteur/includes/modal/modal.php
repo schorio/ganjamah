8d20c44b85c1a179c8817c5d17cd6017319d5b8b
schorio <schorioignace@gmail.com>
2023-06-16 21:12:02 +0300

    reservation-visiteur : fix place disponible

--- visiteur/includes/modal/modal.php
+++ visiteur/includes/modal/modal.php
@@ -26,7 +26,7 @@
 				      			<input name="contact_reservation" type="text" class="form-control" placeholder="Contact">
 				      		</div>
 				      		<div class="form-group">
-								<input name="place_reservation" id="input_1" type="number" class="form-control" placeholder="Nombre de personne">
+								<input name="place_reservation" id="input_1" type="number" class="form-control" placeholder="Nombre de personne (<?php echo $row['PLACE_CIRCUIT']; ?> dispo)" min="1" max="<?php echo $row['PLACE_CIRCUIT']; ?>">
 								<input id="input_11" type="hidden" class="form-control" value="<?php echo $row['PRIX_CIRCUIT']; ?>">
 							</div>
 							<div class="form-group">
